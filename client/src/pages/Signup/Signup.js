8da06bb3ed2bf4109d1e01ace23a55929438d57f
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-11 17:33:18 +0100

    adding the signup  hrml form

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    
+    <div class="signup">
+    <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+       <h1>Inscription <i class="fa-solid fa-user"></i></h1>
+       <form class="form-group" >
+          
+          <input type="text" class="form-control" placeholder="username" />
+          <input type="email" class="form-control" placeholder="Email" />
+          <input type="password" class="form-control" placeholder="Password" />
+          <p>Already have an account? <a href="/">Sign in here</a>
+          </p>
+       </form>
+    </div>
+ </div>
+
+   )
 }
 
 export default Signup
