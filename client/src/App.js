64bb8ef155a394ffdf9bda0a29d265540757b634
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-11 17:02:35 +0100

    putting the skeleton of the client side (components and pages) and fixing the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
  import './App.css';
+ import Signin from './pages/Signin/Signin'
+ import Signup from './pages/Signup/Signup'
+ import Home from './pages/Home/Home'
+ import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-hello world
-    </div>
+    <BrowserRouter>
+    <Routes>
+        <Route  path="/"  element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/logout" element={<Logout />} /> */}
+        
+      </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ client/src/components/CustomButton/CustomButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomButton() {
+  return (
+    <div>CustomButton</div>
+  )
+}
+
+export default CustomButton
