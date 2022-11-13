55972d2afd4b75d3c30951634f1f4d05d32d75c7
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-11-13 16:58:35 +0100

    added a SigninValidation methode to handle signin user's inputs

---
+++ server/validator/SigninValidation.js
@@ -0,0 +1,28 @@
+const validator = require("validator");
+const isEmpty = require("./IsEmpty");
+
+module.exports = function SigninValidation() {
+  let errors = {};
+
+  // Convert empty fields to an empty string so we can use validator
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+
+  // Email checks
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  } else if (!validator.isEmail(data.email)) {
+    errors.email = "Format Email required";
+  }
+
+  // Password checks
+  if (validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
+
+export default SigninValidation;
