2f45b1fa176c1309033e924dcb0711e8718ba9aa
Zhioua Mohamed <zhiouamohamed1989@gmail.com>
2022-12-06 10:42:04 +0100

    Delete Footer.js

--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  const year = new Date().getFullYear();
-
-  return <footer>{`Copyright © Zhioua Mohamed Code ${year}`}</footer>;
-};
-
-export default Footer;
