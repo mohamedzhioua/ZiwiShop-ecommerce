4e6be1c9060ed33306276c2ca8ca9f06df0fa925
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-08-03 14:30:24 +0100

    adding stripe dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Provider store={store}>
         <AuthProvider>
           <PayPalScriptProvider deferLoading={true}>
-
             <App />
           </PayPalScriptProvider>
         </AuthProvider>
