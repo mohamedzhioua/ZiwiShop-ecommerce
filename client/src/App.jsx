fc6649c399d6a1924f544f25505998da39d455f3
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-08-03 14:42:51 +0100

    getting the stripe key and using it to load stripePromise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,26 +12,43 @@ import CartMenu from './components/Cart/index';
 import { ScrollToTop } from './utils/scrollToTop';
 import ScrollToTopBtn from './components/ui/ScrollToTopBtn';
 import Splash from './components/ui/Splash';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useEffect, useState } from 'react';
+import { paymentApi } from './api/PaymentApi';
 
 
 
 function App() {
   const { isInitialized } = useAuth();
   const { theme } = useTheme();
-
+  const [stripeApiKey, setStripeApiKey] = useState("");
+  useEffect(() => {
+    async function fetchStripeApiKey() {
+      const data = await paymentApi.getstripeapikey();
+      setStripeApiKey(data);
+    }
+    fetchStripeApiKey();
+  }, []);
+  const stripePromise = loadStripe(stripeApiKey);
+ 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
-       <div className='content-wrap'>
-        <Navbar />
-      {isInitialized ? <Router /> : <Splash/>}
-        <Toaster position="top-center" />
-        <CartMenu />
-        </div>
-        <Footer />
-         <ScrollToTop />
-        <ScrollToTopBtn />
+        {stripeApiKey && (
+          <Elements stripe={stripePromise}>
+            <div className='content-wrap'>
+              <Navbar />
+              {isInitialized ? <Router /> : <Splash />}
+              <Toaster position="top-center" />
+              <CartMenu />
+            </div>
+            <Footer />
+            <ScrollToTop />
+            <ScrollToTopBtn />
+          </Elements>
+        )}
       </BrowserRouter>
     </ThemeProvider>
 
