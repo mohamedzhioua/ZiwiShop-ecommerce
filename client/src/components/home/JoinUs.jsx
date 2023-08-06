b6431bcdc73fdf6af055cfcbad9a9443a599bad8
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-08-07 00:25:33 +0100

    improving joinUs style

--- client/src/components/home/JoinUs.jsx
+++ client/src/components/home/JoinUs.jsx
@@ -1,59 +1,53 @@
 import {
-    Box, 
-     TextField, Typography
+    Box, Unstable_Grid2 as Grid,
+    TextField, Typography
 } from "@mui/material";
 import CustomButton from "../ui/CustomButton";
 import { Link } from "react-router-dom";
-import { Stack } from "@mui/system";
- 
+
 const JoinUs = () => {
- 
+
     return (
-        <>
-         
-        <Box sx={{
-            maxWidth: '750px', m: 'auto', px: { xs: "20px", xl: "40px" },
-            mt: "100px"
-        }}>
-
-            <Stack  spacing={2} sx={{
-                display: "flex", alignItems: "center", justifyContent: "center",
-                flexDirection: "column"
-            }}>
-
-                 <Typography variant="h2" sx={{
-                    fontWeight: "600",
-                    mb: "10px", textAlign: "center",
-                 }}>
+        <Grid container spacing={1} >
+            <Grid xs={12}>
+                <Typography
+                    variant="h2"
+                    sx={{
+                        fontWeight: "600",
+                        mb: "10px", textAlign: "center",
+                    }}>
                     Subscribe our newsletter to get updates to our latest collections
                 </Typography>
-
-                 <Typography  variant="h5"  sx={{
+            </Grid>
+            <Grid xs={12} md={6}>
+                <Typography variant="h5" sx={{
                     mb: "15px", fontWeight: "500", textAlign: "center",
-                 }}>
+                }}>
                     Get 20% off on your first order just by subscribing to our newsletter!
                 </Typography>
 
-                 <Box sx={{
+                /</Grid>
+            <Grid
+                xs={12}
+                md={6}
+            >
+                <Box sx={{
                     display: "flex", alignItems: "center", justifyContent: "center",
                     gap: "3px", mb: "5px"
                 }}>
-
-                    <Box>
-                        <TextField id="outlined-basic" label="Email Address"
-                            variant="outlined" size="small"
-                            InputLabelProps={{ style: { fontSize: "13px" } }}
-                        />
-                    </Box>
-
-                    <CustomButton    >   
-                            Subscribe
+                    <TextField id="outlined-basic" label="Email Address"
+                        variant="outlined" size="small"
+                        InputLabelProps={{ style: { fontSize: "13px" } }}
+                    />
+                    <CustomButton >
+                        Subscribe
                     </CustomButton>
 
                 </Box>
-
-                 <Typography variant="h5"  sx={{
-                    width: "300px", textAlign: "center",
+            </Grid>
+            <Grid xs={12}>
+                <Typography variant="h5" sx={{
+                    textAlign: "center",
                     wordSpacing: "0.5px"
                 }}>
                     You will be able to unsubscribe at any time.
@@ -62,11 +56,10 @@ const JoinUs = () => {
                         here
                     </Link>
                 </Typography>
+            </Grid>
+        </Grid>
 
-            </Stack>
 
-        </Box>
-        </>
     );
 }
 
