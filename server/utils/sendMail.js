9e2c74719a000c163616a23a78ef5e31cfbbeac8
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-08-10 17:42:29 +0100

    trying to fix server deploy

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,5 +1,5 @@
 const nodemailer = require("nodemailer");
-const hbs = require("nodemailer-express-handlebars");
+const hbs = require('nodemailer-express-handlebars');
 const path = require("path");
 
 const sendMail = async (to, url, name, subject, template) => {
