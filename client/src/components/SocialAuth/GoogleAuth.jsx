b89e0b80462d672f50e0231f43109de63ed0e5ce
mohamedzhioua <zhiouamohamed1989@gmail.com>
2023-08-11 20:25:03 +0100

    deleting not used dependencies and rafactoring the google auth because React Google Login package has been deprecated

--- client/src/components/SocialAuth/GoogleAuth.jsx
+++ client/src/components/SocialAuth/GoogleAuth.jsx
@@ -1,56 +1,41 @@
-import { useEffect } from "react";
-import { GoogleLogin } from "react-google-login";
-import { gapi } from "gapi-script";
 import CustomButton from "../ui/CustomButton";
- import useAuth from "../../hooks/useAuth";
+// import useAuth from "../../hooks/useAuth";
 import { GoogleIcon } from "../icons";
+import { Link as RouterLink } from "react-router-dom";
+import { getGoogleOAuthURL } from "../../utils/getGoogleOAuthURL";
+
+
+
 
 
 const GoogleAuth = () => {
-  const { googleLogin } = useAuth();
+  const googleOAuthURL = getGoogleOAuthURL();
 
+  // const { googleLogin } = useAuth();
 
-  useEffect(() => {
-    function initClient() {
-      gapi.client.init({
-        clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-        scope: "",
-      });
-    }
-    gapi.load("client:auth2", initClient);
-  });
-
-  const responseGoogle = async (response) => {
-     try {
-     await googleLogin(response.tokenId)    
-     } catch (error) {
-      console.log("GOOGLE SIGNIN ERROR", error);
-    }
-  };
+ 
+  // const responseGoogle = async (response) => {
+  //    try {
+  //    await googleLogin(response.tokenId)    
+  //    } catch (error) {
+  //     console.log("GOOGLE SIGNIN ERROR", error);
+  //   }
+  // };
 
   return (
-    <div>
-      <GoogleLogin
-        clientId={`${import.meta.env.VITE_GOOGLE_CLIENT_ID}`}
-        buttonText="Continue with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={"single_host_origin"}
-        render={(renderProps) => (
-          <CustomButton
-            variant="outlined"
-            fullWidth
-            size="large"
-            startIcon={<GoogleIcon/>}
-            onClick={renderProps.onClick}
-            disabled={renderProps.disabled}
-            color="primary"
-          >
-            Continue with Google
-          </CustomButton>
-        )}
-      />
-    </div>
+    <>
+      <CustomButton
+        component={RouterLink}
+        variant="outlined"
+        fullWidth
+        size="large"
+        startIcon={<GoogleIcon />}
+        color="primary"
+        to={googleOAuthURL}
+      >
+        Continue with Google
+      </CustomButton>
+    </>
   );
 };
 
